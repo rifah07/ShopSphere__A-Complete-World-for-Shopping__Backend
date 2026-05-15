ed81f641cd0c46b5da7c196b295a389aa4ee541d
Rifah Sajida Deya <rifahsajida7@gmail.com>
2026-05-15 12:11:49 +0600

    feat; authorization add in scrapping route

--- backend/src/modules/scraping/scraping.routes.ts
+++ backend/src/modules/scraping/scraping.routes.ts
@@ -5,16 +5,27 @@ import {
   scrapeSingleURL,
 } from "./scraping.controller";
 import auth from "../../middlewares/authMiddleware";
+import authorize from "../../middlewares/authorize";
 
 const router = Router();
 
 // GET  /api/scraping/price-suggestion/:productId
-router.get("/price-suggestion/:productId", auth, getPriceSuggestionForProduct);
+router.get(
+  "/price-suggestion/:productId",
+  auth,
+  authorize("seller", "admin"),
+  getPriceSuggestionForProduct,
+);
 
 // POST /api/scraping/price-suggestion/manual
-router.post("/price-suggestion/manual", auth, getManualPriceSuggestion);
+router.post(
+  "/price-suggestion/manual",
+  auth,
+  authorize("seller", "admin"),
+  getManualPriceSuggestion,
+);
 
 // POST /api/scraping/scrape-url
-router.post("/scrape-url", auth, scrapeSingleURL);
+router.post("/scrape-url", auth, authorize("seller", "admin"), scrapeSingleURL);
 
 export default router;
